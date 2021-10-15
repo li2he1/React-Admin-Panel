528ec69b4ffd297bd2d1d1fda1a40e27ea0fe517
li2he1 <51969544+li2he1@users.noreply.github.com>
2021-10-14 22:25:44 -0700

    user list and edit user

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 import "./app.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 export default function App() {
   return (
-    <div className="App">
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
